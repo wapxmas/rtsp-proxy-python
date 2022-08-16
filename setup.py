40b440084e3564608d7a912995e9159206fbd7e9
sergei <s_yashin@head-point.ru>
2022-08-17 02:10:34 +0300

    + setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rtsp-proxy-python',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/wapxmas/rtsp-proxy-python',
+    license='MIT',
+    author='Sergei Iashin',
+    author_email='yashin.sergey@gmail.com',
+    description='RTSP Proxy'
+)
